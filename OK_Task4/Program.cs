ca1d12f5dd4d2ce26881de02ec8fada40fa25d0f
Olga Kolisnyk <Olga.Kolisnyk@teaminternational.com>
2021-03-05 10:34:39 +0200

    Add project files.

---
+++ OK_Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OK_Task4
+{
+    class Program
+    {
+        public static bool IsPalindrome(string str)
+        {
+            int i = 0, j = str.Length - 1;
+
+            while (i < j)
+                if (str[i++] != str[j--]) return false;
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input word: ");
+            
+            String str = Console.ReadLine();
+
+            if (IsPalindrome(str) == true) Console.WriteLine("Word is a palindrome");
+            else Console.WriteLine("World is not a palindrome");
+           
+            Console.ReadKey();
+        }
+    }
+}
